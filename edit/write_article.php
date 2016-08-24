3e5f6cc141e6cd91562c068cfe7d64ee678dfea7
fujimoto <basssaudaji@yahoo.co.jp>
2016-08-24 21:35:46 +0900

    improved editor

--- edit/write_article.php
+++ edit/write_article.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 
 <html>
+<head>
 <title>記事一覧</title>
 <meta charset="UTF-8");
 <meta name="viewport", content="width=device-width, initial-scale=1.0">
-
+<script src="/marked/lib/marked.js"></script>
+<script src="/Blog/Blog_config/my.js"></script>
 <?php
     ini_set("display_errors", 1);
     require_once '../db_access.php';
@@ -19,26 +21,33 @@
     $stmh->execute();
     $row = $stmh->fetch(PDO::FETCH_ASSOC);
 ?>
-<html>
-    <head>
         <title><?=$row["title"] ?></title>
     </head>
-    <body>
+    <body onload="enable_tab('input_field')">
+    <table>
+        <tr><td>
         <form method="post" action="write_db.php" name="form1" >
             Title:<br>
             <input type="text" name="title" value="<?=$row["title"]?>" style="width:500"><br><br>
             Content:<br>
-            <textarea style="width: 800px; height:200px" name="content"><?php
+            <textarea style="width: 800px; height:200px" oninput="change_txtarea();"
+                name="content" class="input_field" id="content"><?php
                 print($row["content"]);
             ?></textarea>
             <br><br>
             Detail:<br>
-            <textarea style="width: 800px; height:500px" name="detail"><?php
+            <textarea style="width: 800px; height:500px" oninput="change_txtarea();"
+                name="detail" class="input_field" id="detail"><?php
                 print($row["detail"]);
                 ?></textarea><br><br>
             <input type="submit" value="送信">
             <input type="hidden" name="id" value="<?=$_GET["id"]?>">
         </form>
+        </td></tr>
+        <tr><td style="border: dashed 1px; padding: 5px;">
+            <div id="prev_field">aaaa</div>
+        </td></tr>
+    </table>
     </body>
 </html>
 
@@ -62,7 +71,6 @@
             <input type="submit" value="送信">
         </form>
     </body>
-</html>
 
 
 <?php
